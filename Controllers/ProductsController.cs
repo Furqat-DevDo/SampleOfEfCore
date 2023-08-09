1334e9a336dcab7f6850c9632b1570d6866f656d
Newdeveloper-c <turdimurodovnumonjon@gmail.com>
2023-08-09 19:10:43 +0500

    some documentation was added

--- Controllers/ProductsController.cs
+++ Controllers/ProductsController.cs
@@ -14,8 +14,13 @@ public class ProducsController : ControllerBase
     public ProducsController(IProductService productService)
     {
         _productService = productService;
-    }    
-    
+    }
+
+    /// <summary>
+    /// Siz yangi product yaratishiz mumkin!
+    /// </summary>
+    /// <param name="product"></param>
+    /// <response code="200">Returns the newly created product</response>
     [HttpPost]
     public async Task<IActionResult> CreateProductAsync(CreateProductRequest request)
     {
@@ -25,22 +30,37 @@ public class ProducsController : ControllerBase
             Ok(response);
     }
 
-
+    /// <summary>
+    /// Id orqali productni olishingiz mumkin!
+    /// </summary>
+    /// <param name="product"></param>
+    /// <response code="200">Returns the product with Id</response>
+    /// <response code="404">Returns null when product was not found</response>
     [HttpGet("{id}")]
     public async Task<IActionResult> GetProductByIdAsync(uint id)
     {
         var response = await  _productService.GetProductByIdAsync((int)id);
-        return response is null ? NotFound() : Ok(response);
+        return response is null ? NotFound(null) : Ok(response);
     }
 
-
+    /// <summary>
+    /// Barcha productlarni olishingiz mumkin!
+    /// </summary>
+    /// <param name="product"></param>
+    /// <response code="200">Returns all products</response>
+    /// <response code="404">Returns null when products was not found</response>
     [HttpGet]
     public async Task<IActionResult> GetAllProductsAsync()
     {
         return Ok(await _productService.GetAllProductsAsync());
     }
 
-
+    /// <summary>
+    /// Id orqali productni o'chirishingiz mumkin!
+    /// </summary>
+    /// <param name="product"></param>
+    /// <response code="200">Deletes the product with Id</response>
+    /// <response code="404">Returns null when product was not found</response>
     [HttpDelete("{id}")]
     public async Task<IActionResult> DeleteProductAsync(uint id)
     {
@@ -48,7 +68,12 @@ public class ProducsController : ControllerBase
         return result ? Ok(result) : NotFound();    
     }
 
-
+    /// <summary>
+    /// Id orqali productni o'zgartirishingiz mumkin!
+    /// </summary>
+    /// <param name="product"></param>
+    /// <response code="200">Returns updated product with Id</response>
+    /// <response code="404">Returns null when product was not found</response>
     [HttpPut("{id}")]
     public async Task<IActionResult> UpdateProductAsync(uint id, [FromBody] UpdateProductRequest request)
     {
