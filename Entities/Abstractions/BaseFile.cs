abce7f9fffc8daee12263deab5097626e14e9a6c
Furqat-DevDo <furqata93@gmail.com>
2023-08-15 21:20:04 +0500

    Ok

--- Entities/Abstractions/BaseFile.cs
+++ Entities/Abstractions/BaseFile.cs
@@ -2,10 +2,8 @@
 
 public abstract class BaseFile  : BaseEntity
 {
-    public Guid Id { get; set; }
+    public new Guid Id { get; set; }
     public virtual string Extension { get; set; } = null!;
     public virtual long Size { get; set; }
     public virtual string Src { get; set; } = null!;
-    
-
 }
