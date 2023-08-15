814dc0a88e6148082aea9f31ea141ce27bfbe1a8
BehruzbekUmarov <bekhruz476@gmail.com>
2023-08-15 06:52:25 -0800

    produceResponseType was added

--- Controllers/CategoryImagesController.cs
+++ Controllers/CategoryImagesController.cs
@@ -1,4 +1,5 @@
 ﻿using EfCore.Models.Requests;
+using EfCore.Models.Responses;
 using EfCore.Services;
 using EfCore.Services.Interfaces;
 using Microsoft.AspNetCore.Mvc;
@@ -21,26 +22,37 @@ public class CategoryImagesController : ControllerBase
     /// <summary>
     /// Here you can create new image.
     /// </summary>
+    /// <param name="request">Field for adding new image</param>
+    /// <param name="id">Id of existing product</param>
     /// <response code="201">Returns the newly created image</response>
     /// <response code="400">Returns when there was unable to create image</response>
+    /// <response code="404">Returns NULL when there was unable to find image which id = Id</response>
+    /// <response code="500">Returns when there was unable to add new image</response>
     [HttpPost]
+    [ProducesResponseType(typeof(GetCategoryImageResponse), StatusCodes.Status201Created)]
     public async Task<IActionResult> CreateAsync(int id, [FromForm] CreateCategoryImageRequest request)
     {
+        var category = await _categoryService.GetCategoryByIdAsync(id);
+        if (category is null) return NotFound(null);
+
         if (!ModelState.IsValid)
         {
             return BadRequest(ModelState);
         }
         var result = await _categoryImageService.CreateAsync(id, request);
         var routValue = new { id = result.CategoryId };
-        return CreatedAtRoute(routValue, result);
+        return result is not null ? CreatedAtRoute(routValue, result) : new StatusCodeResult(StatusCodes.Status500InternalServerError);
     }
 
     /// <summary>
     /// Here you can get the image.
     /// </summary>
+    /// <param name="id"> Id of existing product</param>
+    /// <param name="filePath"> Path of image</param>
     /// <response code="200">Returns the newly created image</response>
     /// <response code="404">Returns when there was unable to get image</response>
     [HttpGet("direct")]
+    [ProducesResponseType(typeof(GetCategoryImageResponse), StatusCodes.Status200OK)]
     public async Task<IActionResult> GetFileFromPath(int id, string filePath)
     {
 
@@ -59,9 +71,12 @@ public class CategoryImagesController : ControllerBase
     /// <summary>
     /// Here you can get file of the image.
     /// </summary>
+    /// <param name="id">Id of existing product</param>
+    /// <param name="filePath">Path of image</param>
     /// <response code="200">Returns the newly file of created image</response>
     /// <response code="404">Returns when there was unable to get image</response>
     [HttpGet("download")]
+    [ProducesResponseType(typeof(GetCategoryImageResponse), StatusCodes.Status200OK)]
     public async Task<IActionResult> GetFileFromPathDownload(int id, string filePath)
     {
         var image = await _categoryService.GetCategoryByIdAsync(id);
@@ -79,9 +94,11 @@ public class CategoryImagesController : ControllerBase
     /// <summary>
     /// Here you can get the image by id.
     /// </summary>
+    /// <param name="id">Id of existing product</param>
     /// <response code="200">Returns the newly created image</response>
     /// <response code="404">Returns when there was unable to get image</response>  
     [HttpGet]
+    [ProducesResponseType(typeof(IEnumerable<GetCategoryImageResponse>), StatusCodes.Status200OK)]
     public async Task<IActionResult> GeCategoriesImages(int id)
     {
         var image = await _categoryService.GetCategoryByIdAsync(id);
@@ -93,10 +110,13 @@ public class CategoryImagesController : ControllerBase
 
     /// <summary>
     /// Here you can delete existing image by its Id.
-    /// </summary>d
+    /// </summary>
+    /// <param name="id">Id of existing product</param>
+    /// <param name="fileId">Id of existing product image</param>
     /// <response code="200">Deletes the image with Id and returns true</response>
     /// <response code="404">Returns false when image was not found</response>
     [HttpDelete("{fileId}")]
+    [ProducesResponseType(typeof(GetCategoryImageResponse), StatusCodes.Status200OK)]
     public async Task<IActionResult> DeleteCategoryImage(int id, Guid fileId)
     {
         var image = await _categoryService.GetCategoryByIdAsync(id);
