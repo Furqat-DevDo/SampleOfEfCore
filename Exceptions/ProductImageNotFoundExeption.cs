a940b9dcd0d52126d4503e3d1b5f38448a9b0041
BehruzbekUmarov <bekhruz476@gmail.com>
2023-08-16 08:04:19 -0800

    exeptions added

---
+++ Exceptions/ProductImageNotFoundExeption.cs
@@ -0,0 +1,24 @@
+﻿using System.Runtime.Serialization;
+
+namespace EfCore.Exceptions
+{
+    [Serializable]
+    internal class ProductImageNotFoundExeption : Exception
+    {
+        public ProductImageNotFoundExeption()
+        {
+        }
+
+        public ProductImageNotFoundExeption(string? message) : base(message)
+        {
+        }
+
+        public ProductImageNotFoundExeption(string? message, Exception? innerException) : base(message, innerException)
+        {
+        }
+
+        protected ProductImageNotFoundExeption(SerializationInfo info, StreamingContext context) : base(info, context)
+        {
+        }
+    }
+}
