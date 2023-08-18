0fb3646b570cc6f95c24b9477289d0fc3a8f52f9
Furqat-DevDo <furqata93@gmail.com>
2023-08-18 20:50:54 +0500

    Attribute Delegate ishlatadigan bo'ldi

--- Attributes/ErrorHandlingFilterAttribute.cs
+++ Attributes/ErrorHandlingFilterAttribute.cs
@@ -16,23 +16,28 @@ public class ErrorHandlingFilterAttribute : ExceptionFilterAttribute
         {
             Type = "https://tools.ietf.org/html/rfc7231#section-6.6.1",
             Title = exception.Message,
+            Detail = exception.ToString(),
             Status = (int)HttpStatusCode.InternalServerError,
         };
 
-        switch (exception)
+        var exceptionHandlers = new Dictionary<Type, Action<ProblemDetails>>
         {
-            case BaseNotFoundException :
-                problemDetails.Type = "https://datatracker.ietf.org/doc/html/rfc7231#section-6.5.4";
-                problemDetails.Status = (int)HttpStatusCode.NotFound;
-                problemDetails.Title = exception.Message;
-                break;
-
-            default:
-                break;
-        }
-
+            { typeof(BaseNotFoundException), (problemDetails) =>
+                {
+                    problemDetails.Type = "https://datatracker.ietf.org/doc/html/rfc7231#section-6.5.4";
+                    problemDetails.Status = (int)HttpStatusCode.NotFound;
+                    problemDetails.Title = exception.Message;
+                    problemDetails.Detail = exception.ToString();
+                }
+            },
+            // add additional exception types and handlers as needed
+        };
         
-
+        if (exceptionHandlers.TryGetValue(exception.GetType(), out var handler))
+        {
+            handler(problemDetails);
+        }
+        
         context.Result = new ObjectResult(problemDetails);
         context.ExceptionHandled = true;
     }
